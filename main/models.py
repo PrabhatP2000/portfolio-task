7c422800b8ae8984eafb5c63358d5c6e127ec863
Prabhat Pandey <pandeyprabhat206@gmail.com>
2021-09-07 18:29:47 +0530

    Contact form backend added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactForm(models.Model):
+    NAME=models.CharField(max_length=20)
+    EMAIL=models.EmailField()
+    SUBJECT=models.CharField(max_length=50)
+    MESSAGE=models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.NAME
--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .models import ContactForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'portfolio.html')
+
+def contactForm(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        contactform =ContactForm(NAME=name,EMAIL=email,SUBJECT=subject,MESSAGE=message)
+        contactform.save()
+        return render(request, 'portfolio.html')
+        
+    else:
+        return HttpResponse("Please try again Error 404")
